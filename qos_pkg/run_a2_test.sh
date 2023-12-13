700d9283c64df83f1b10d2696e04202282b0d8d8
DominikUrbaniak <126696875+DominikUrbaniak@users.noreply.github.com>
2023-12-13 12:26:38 +0100

    Add files via upload

--- qos_pkg/run_a2_test.sh
+++ qos_pkg/run_a2_test.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+sleep 2
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+sleep 2
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+sleep 2
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
 
-timeout 10 ros2 run qos_pkg poses_a2_sub_pub
+sleep 2
+
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
+
+sleep 2
+
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
+
+sleep 2
+
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
+
+sleep 2
+
+timeout 8 ros2 run qos_pkg poses_a2_sub_pub
