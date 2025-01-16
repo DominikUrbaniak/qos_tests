3b2da5126a8095843bde4c970fb34b204293f27e
DominikUrbaniak <126696875+DominikUrbaniak@users.noreply.github.com>
2025-01-16 12:14:46 +0100

    Add files via upload

---
+++ run_5g_madar.sh
@@ -0,0 +1,6 @@
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/dominik.urbaniak/5g_config/network_5g.xml
+export ROS_DOMAIN_ID=20
+fastdds discovery -i 0 -l 127.0.0.1 -p 11811 &
+fastdds discovery -i 1 -l 10.5.39.10 -p 11812 &
+#export ROS_DISCOVERY_SERVER="127.0.0.1:11811;10.5.39.40:11812;10.5.24.66:11812"
+
